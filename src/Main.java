fc85ba46d3e23b93777d7f65d85e60cc8ed4db2d
lillian880205 <lillian880205@gmail.com>
2017-09-14 13:28:19 +0800

    second

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ import java.awt.peer.SystemTrayPeer;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.print("My First Java Code!!");
+        System.out.print("PUPU's First Java Code!!");
     }
 }
